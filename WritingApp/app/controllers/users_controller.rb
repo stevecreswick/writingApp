4d841ff3a195654efbca9f7535dd8ae969087373
Steven Creswick <screswick@gmail.com>
2015-09-24 14:14:15 -0400

    adds new user form

--- WritingApp/app/controllers/users_controller.rb
+++ WritingApp/app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+
+  private
+  def user_params
+    # params.require(:user).permit(:username, :password)
+  end
+
 end
