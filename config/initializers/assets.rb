a35dac26678afd66fa4b0d833113db232c4fa741
Steve Creswick <screswick@gmail.com>
2016-01-05 13:00:07 -0500

    herkou font fix 1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( main.js )
 Rails.application.config.assets.precompile += %w( profile.js )
