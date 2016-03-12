9b1df0b16abbdaf8969e6fad78e8760507e712a4
Steve Creswick <screswick@gmail.com>
2016-03-12 16:08:24 -0500

    Refactor post preview render function

--- app/assets/javascripts/views/posts/components/post-preview.js
+++ app/assets/javascripts/views/posts/components/post-preview.js
@@ -5,13 +5,7 @@ app.PostPreview = Backbone.View.extend({
 
   render: function(){
     this.$el.empty();
-    var html = this.template( this.model.toJSON() );
-    console.log(this.model);
-
-    this.$el.append( $html );
-
-    var $html = $( html );
-    this.$el.append( $html );
+    this.$el.append( $( this.template( this.model.toJSON() ) ) );
   }
 
 });
