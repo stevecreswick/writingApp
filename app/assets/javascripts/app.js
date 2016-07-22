6264954224f981deb412adb088d5afae50350687
Steve Creswick <screswick@gmail.com>
2016-07-21 23:40:39 -0400

    Remove onload function

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,32 +1,9 @@
-var app = app || {};
+var app = {};
+app.token = document.getElementById( 'api-token' ).value;
 
-  app.token = $('#api-token').val();
-  $.ajaxSetup({
-    headers:{
-      "accept": "application/json",
-      "token": app.token
-    }
-  });
-
-var do_on_load = function() {
-
-  // Responsible for clearing and rendering page
-  app.pagePainter = new app.PageView({
-    el: $('#main-page')
-  });
-
-
-  $.material.init()
-  app.pagePainter.render();
-  app.pagePainter.renderPostsPage();
-
-  // app.pagePainter.renderPosts( app.pagePainter.currentGenre, app.pagePainter.currentPage );
-  // app.pagePainter.renderNavBar();
-  // app.pagePainter.currentUser();
-
-
-
-
-}
-
-$(document).ready(do_on_load);
+$.ajaxSetup({
+  headers:{
+    "accept": "application/json",
+    "token": app.token
+  }
+});
