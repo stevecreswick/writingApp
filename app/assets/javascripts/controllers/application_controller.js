c9ee7acfc17149199cecb6a45c02f22aacbac947
Steve Creswick <screswick@gmail.com>
2016-07-24 16:06:05 -0400

    Call for currentUser on app load

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -2,12 +2,31 @@ console.log('loaded appplicat');
 
 angular.module('writeAway')
   .controller(
-    'ApplicationController', [
-    '$scope',
-    function( $scope ){
+    'ApplicationController',
+    [
+      '$scope', 'User',
+      function( $scope, User ){
 
-      $scope.apiPage = 0;
-      $scope.currentPage = $scope.apiPage + 1;
+        $scope.apiPage = 0;
+        $scope.currentPage = $scope.apiPage + 1;
 
-
-  }]);
+        User.getUser(
+          parseInt(
+            document.
+            getElementById(
+              'current_id'
+            ).value
+          )
+        ).
+        then(
+          function ( user ) {
+            $scope.currentUser = user.data;
+            console.log($scope.currentUser);
+          },
+          function ( error ) {
+              $scope.status = 'Unable to load user: ' + error.message;
+            }
+        );
+      }
+    ]
+  );
