1f3a08a0b3eafdfd69b49927d3e60886e07b1a12
Steve Creswick <screswick@gmail.com>
2016-07-24 16:08:25 -0400

    Remove console logs

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,10 +1,7 @@
-console.log('loaded home');
-
 angular.module('writeAway')
   .controller(
     'HomeController', [
     '$scope',
     function( $scope ){
-      console.log('loaded the home controllerrrr');
 
   }]);
