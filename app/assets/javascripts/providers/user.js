babdcd97f2f185eb5db42543bae51e17412c106d
Steve Creswick <screswick@gmail.com>
2016-07-30 14:55:06 -0400

    Fix spacing in user factory

--- app/assets/javascripts/providers/user.js
+++ app/assets/javascripts/providers/user.js
@@ -8,23 +8,23 @@ angular.module('writeAway')
         var User = {};
 
         User.getUsers = function (page) {
-            return $http.get(urlBase + '/' + 0);
+          return $http.get(urlBase + '/' + 0);
         };
 
         User.getUser = function (id) {
-            return $http.get(urlBase + '/show/' + id);
+          return $http.get(urlBase + '/show/' + id);
         };
 
         User.insertUser = function ( user ) {
-            return $http.post(urlBase, cust);
+          return $http.post(urlBase, cust);
         };
 
         User.updateUser = function ( user ) {
-            return $http.put(urlBase + '/' + user.ID, user)
+          return $http.put(urlBase + '/' + user.ID, user)
         };
 
         User.deleteUser = function ( id ) {
-            return $http.delete(urlBase + '/' + id);
+          return $http.delete(urlBase + '/' + id);
         };
 
         return User;
