d0af9731ac356ceae597d91a87d914aae226f364
Steve Creswick <screswick@gmail.com>
2016-08-16 01:28:29 -0400

    Update prompt types and descriptions

--- app/assets/javascripts/controllers/writing_prompt_controller.js
+++ app/assets/javascripts/controllers/writing_prompt_controller.js
@@ -4,9 +4,23 @@ angular.module('writeAway')
     '$scope', '$rootScope', 'Prompt',
     function( $scope, $rootScope, Prompt ){
       $scope.wordCount = 100;
-      $scope.promptType = 'Start My Sentences';
-      $scope.description = {
-        'Start My Sentences': 'Write a story using a random word to start each sentence.  Typing a period and space will generate a new word.'
+      $scope.writingPost = false;
+      $scope.promptTypes = [
+        { 'name': 'any', 'value': 'Any' },
+        { 'name': 'start-my-sentences', 'value': 'Start My Sentences' },
+        { 'name': 'answer-what-if', 'value': 'Answer What If' },
+        { 'name': 'classic-first-sentence', 'value': 'Classic First Sentence' },
+        { 'name': 'reddit', 'value': 'Reddit' }
+      ];
+
+      $scope.currentPromptType = $scope.promptTypes[ 0 ];
+
+      $scope.promptDescription = {
+        'any': '',
+        'start-my-sentences': 'Write a story using a random word to start each sentence.  Typing a period and space will generate a new word.',
+        'answer-what-if': 'Write a story surrounding a \'what if\' statement',
+        'classic-first-sentence': 'Write a story starting with the first sentence of a classic novel.',
+        'reddit': 'Answer a random prompt submitted to /r/writingprompts.'
       };
 
       $scope.getPrompt = function() {
@@ -21,6 +35,25 @@ angular.module('writeAway')
                 $scope.status = 'Unable to load prompt: ' + error.message;
               }
           );
+      };
+
+      $scope.getPromptList = function() {
+        Prompt.
+          getPrompts( $scope.promptPage ).
+          then(
+            function ( prompt ) {
+              $scope.writingPrompt = prompt.data;
+            },
+            function ( error ) {
+                $scope.status = 'Unable to load prompt: ' + error.message;
+              }
+          );
+      };
+
+      $scope.startWriting = function() {
+        console.log('start writing');
+        $scope.writingPost = !$scope.writingPost;
+        // $rootScope.navigate( '/new-post' );
       };
     }
   ]
